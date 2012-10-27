e911c67c2cf7d7692dd212a1b53f2bfb7d56f666
Rodrigo Silveira <webmaster@rodrigo-silveira.com>
2012-10-27 17:41:38 -0600

    Added some color, and refactored Shape object slightly

--- shaders/simple-vert.glsl
+++ shaders/simple-vert.glsl
@@ -1,8 +1,12 @@
 attribute vec3 aVertPos;
+attribute vec4 aVertColor;
 
 uniform mat4 umModelView;
 uniform mat4 umProjection;
 
+varying vec4 vColor;
+
 void main(void) {
+   vColor = aVertColor;
    gl_Position = umProjection * umModelView * vec4(aVertPos, 1.0);
 }
